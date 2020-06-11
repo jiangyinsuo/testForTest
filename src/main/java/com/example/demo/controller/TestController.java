84f8d799d319c862063aab8a90de30a315beb94f
jiangys <jys1121@163.com>
2020-06-11 23:09:02 +0800

    1、添加一个controller

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jys
+ * @date 2020/6/11 23:08
+ */
+@RestController
+public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+
+}
