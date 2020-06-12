abb19548ef9ac60cb34106efdd6156b9f2470bd9
jiangys <jys1121@163.com>
2020-06-12 13:26:34 +0800

    hello

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,5 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     private final Logger logger = LoggerFactory.getLogger(TestController.class);
 
+    @GetMapping("hello")
+    public void hello() {
+        logger.info("hello");
+    }
 
 }
